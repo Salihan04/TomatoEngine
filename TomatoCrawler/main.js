83e264e925d39812cef34ed5e272e7252fb67d74
soelynn <soelynn007@gmail.com>
2016-02-01 11:12:21 +0800

    Fixed the crawler for more reliable download.

--- TomatoCrawler/main.js
+++ TomatoCrawler/main.js
@@ -2,12 +2,17 @@ const util = require('util');
 const low = require('lowdb')
 const storage = require('lowdb/file-sync')
 const db = low('tomato_db.json', { storage })
+var forEach = require('async-foreach').forEach;
 var uuid = require('node-uuid');
 var Xray = require('x-ray');
+var request = require('sync-request');
+var cheerio = require('cheerio')
 
-function downloadMovieReview(movie_name) {
+function extractMovieReviews(url) {
   var x = Xray();
-  x(util.format('http://www.rottentomatoes.com/m/%s/reviews/?type=user', movie_name),
+  var res = request('GET', url);
+
+  x(res.getBody(),
     '.review_table_row',
     [
       {
@@ -23,16 +28,62 @@ function downloadMovieReview(movie_name) {
 }
 
 var xray = Xray();
-xray('http://www.rottentomatoes.com/top/bestofrt/?year=2014',
+xray('http://www.rottentomatoes.com/top/bestofrt/?year=2015',
   'table',
   ['.articleLink@href']
 )(function(error, links) {
 
-  for(var i=0; i<links.length; i++) {
-    var link = links[i];
-    link = link.replace('http://www.rottentomatoes.com/m/', '');
-    link = link.replace('/', '');
-    downloadMovieReview(link);
-  }
+  forEach(links, function(link, index, links) {
+      var movie_name = link.replace('http://www.rottentomatoes.com/m/', '');
+      movie_name = movie_name.replace('/', '');
+
+      link = util.format('http://www.rottentomatoes.com/m/%s/reviews/?type=user', movie_name);
+      console.log("[STARTED] " + link);
+
+      while(true) {
+        var res = request('GET', link);
+
+        while(res.statusCode !== 200) {
+          res = request('GET', link);
+          console.log("[RETRY] " + link);
+        }
+
+        var $ = cheerio.load(res.getBody('utf-8'));
+
+        // Pagination
+        nextPage = $('.btn-primary-rt[href]').last().attr('href');
+        if(!nextPage || nextPage == '#') {
+          break;
+        }
+
+        var x = Xray();
+        x($('.review_table').html(),
+          '.review_table_row',
+          [
+            {
+              reviewer: x('.articleLink', '@html'),
+              reviewer_url: x('.articleLink', '@href'),
+              review: x('.review_table_row', '.user_review'),
+              rating: x('.scoreWrapper', 'span@class'),
+              date: x('.subtle', '@html')
+            }
+          ])(function(error, reviews) {
+            if(reviews) {
+              for(var i=0; i<reviews.length; i++) {
+                var review = reviews[i];
+                review['id'] = uuid.v4();
+                review['movie'] = movie_name;
+
+                db('reviews').push(review);
+              }
+            }
+          });
+
+        console.log("[DONE] " + link);
+
+        nextPage = 'http://www.rottentomatoes.com' + nextPage;
+        link = nextPage;
+      }
+  });
 
 })
