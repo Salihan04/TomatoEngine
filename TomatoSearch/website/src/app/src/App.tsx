3158a39980f93161193634b796bcfe2d9a3ef4a4
Jun Yi Hee <junyi.hjy@gmail.com>
2016-02-12 13:31:14 +0800

    Update website to reflect latest changes in searchkit

--- TomatoSearch/website/src/app/src/App.tsx
+++ TomatoSearch/website/src/app/src/App.tsx
@@ -17,23 +17,26 @@ import {
   SearchkitProvider,
   SearchkitManager,
   NoHits,
-  RangeFilter
+  RangeFilter,
+  InitialLoader
 } from "searchkit";
 
-require("./../styles/index.scss");
+import "./../styles/customisations.scss";
+import "searchkit/release/theme.css";
 
-class MovieHits extends Hits {
-  renderResult(result:any) {
-    let url = "http://www.rottentomatoes.com/m/" + result._source.review.movie
-    return (
-      <div className={this.bemBlocks.item().mix(this.bemBlocks.container("item"))} key={result._id}>
-        <a href={url} target="_blank">
-          <div className={this.bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:_.get(result,"highlight.review.review",false) || result._source.review.review}}>
-          </div>
-        </a>
-      </div>
-    )
-  }
+const MovieHitsItem = (props)=> {
+  const {bemBlocks, result} = props
+  let url = "http://www.rottentomatoes.com/m/" + result._source.review.movie
+  return (
+    <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
+      <a href={url} target="_blank">
+        <div data-qa="review" className={bemBlocks.item("review")} dangerouslySetInnerHTML={{__html:_.get(result,"highlight.review.review",false) || result._source.review.review}}>
+        </div>
+      </a>
+      <p data-qa="movie" className={bemBlocks.item("movie")} dangerouslySetInnerHTML={{__html:_.get(result,"highlight.review.movie",false) || result._source.review.movie}}>
+      </p>
+    </div>
+  )
 }
 
 export class App extends React.Component<any, any> {
@@ -41,9 +44,9 @@ export class App extends React.Component<any, any> {
   searchkit:SearchkitManager
 
   constructor() {
-    const host = "https://elasticsearch:qwerty123@tomato.ga:9999"
+    const host = "https://tomato.ga:9999"
     this.searchkit = new SearchkitManager(host, {
-      basicAuth: "elasticsearch:qwerty123"
+      basicAuth: "elasticsearch:soelynnlovestomatoes"
     })
     this.searchkit.translateFunction = (key)=> {
       return {"pagination.next":"Next Page"}[key]
@@ -55,47 +58,55 @@ export class App extends React.Component<any, any> {
 
     return (
       <div>
-      <SearchkitProvider searchkit={this.searchkit}>
-      <div>
-        <div className="layout">
-          <div className="layout__search-box">
-            <SelectedFilters/>
-            <SearchBox translations={{"searchbox.placeholder":"search movies"}} queryOptions={{"minimum_should_match":"70%"}} autofocus={true} searchOnChange={true} queryFields={["actors^1","type^2","languages","title^5", "genres^2"]}/>
-          </div>
+        <SearchkitProvider searchkit={this.searchkit}>
+          <div>
+            <div className="layout">
+              <div className="layout__top-bar top-bar">
+                <div className="top-bar__content">
+                  <div className="my-logo">Searchkit Acme co</div>
+                  <SearchBox translations={{"searchbox.placeholder":"search movies"}} queryOptions={{"minimum_should_match":"70%"}} autofocus={true} searchOnChange={true} queryFields={["review.review^1","review.title^2"]}/>
+                </div>
+              </div>
 
-    			<div className="layout__filters">
-    				<ResetFilters />
-            <RangeFilter min={0} max={50} field="review.rating" id="rating" title="Metascore" showHistogram={true}/>
-            <RefinementListFilter translations={{"facets.view_more":"View more writers"}} id="writers" title="Writers" field="review.movie" operator="OR" size={10}/>
-            {/*<HierarchicalMenuFilter fields={["type.raw", "genres.raw"]} title="Categories" id="categories"/>
-            <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
-            <RefinementListFilter id="actors" title="Actors" field="actors.raw" operator="AND" size={10}/>
+              <div className="layout__body">
+          			<div className="layout__filters">
+          				<ResetFilters />
+                  <RangeFilter min={0} max={50} field="review.rating" id="rating" title="Metascore" showHistogram={true}/>
+                  <RefinementListFilter translations={{"facets.view_more":"View more movies"}} id="movies" title="Movies" field="review.movie" operator="OR" size={10}/>
+                </div>
 
-    				<RefinementListFilter id="countries" title="Countries" field="countries.raw" operator="OR" size={10}/>
-            <NumericRefinementListFilter id="runtimeMinutes" title="Length" field="runtimeMinutes" options={[
-              {title:"All"},
-              {title:"up to 20", from:0, to:20},
-              {title:"21 to 60", from:21, to:60},
-              {title:"60 or more", from:61, to:1000}
-            ]}/>*/}
+                <div className="layout__results results-list">
+
+                  <div className="results-list__action-bar action-bar">
+
+                    <div className="action-bar__info">
+              				<HitsStats translations={{
+                        "hitstats.results_found":"{hitCount} results found"
+                      }}/>
+              				<SortingSelector options={[
+              					{label:"Relevance", field:"_score", order:"desc",defaultOption:true},
+              					{label:"Latest Releases", field:"review.released", order:"desc"},
+              					{label:"Earliest Releases", field:"review.released", order:"asc"}
+              				]}/>
+              			</div>
+
+                    <div className="action-bar__filters">
+                      <SelectedFilters/>
+                      <ResetFilters/>
+                    </div>
+
+                  </div>
+                  <Hits hitsPerPage={10} highlightFields={["review.review"]}
+                  itemComponent={MovieHitsItem}/>
+                  <NoHits suggestionsField={"review.review"}/>
+                  <InitialLoader/>
+          				<Pagination showNumbers/>
+          			</div>
+              </div>
+        			<a className="view-src-link" href="https://github.com/searchkit/searchkit-demo/blob/master/src/app/src/App.tsx">View source »</a>
+        		</div>
           </div>
-    			<div className="layout__results-info">
-    				<HitsStats/>
-    				<SortingSelector options={[
-    					{label:"Relevance", field:"_score", order:"desc"},
-    					{label:"Latest Releases", field:"review.released", order:"desc"},
-    					{label:"Earliest Releases", field:"review.released", order:"asc"}
-    				]}/>
-    			</div>
-    			<div className="layout__results">
-    				<MovieHits hitsPerPage={10} highlightFields={["review.review"]}/>
-            <NoHits suggestionsField={"review.review"}/>
-    				<Pagination/>
-    			</div>
-    			<a className="view-src-link" href="https://github.com/searchkit/searchkit-demo/blob/master/src/app/src/App.tsx">View source »</a>
-    		</div>
-      </div>
-      </SearchkitProvider>
+        </SearchkitProvider>
       </div>
 	)}
 
