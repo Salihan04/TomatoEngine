dfe251eb05ddd3708763421cd43ebe959e594db8
Nasruddin <nasruddinAaidil@gmail.com>
2016-04-07 13:47:05 +0800

    fixed preprocess2 will be testing next

--- TomatoClassifier/preprocess_2.py
+++ TomatoClassifier/preprocess_2.py
@@ -42,6 +42,9 @@ def generate_feature_key_capture(feature_sets):
 
 
 def get_feature_keys(word, feature_keys):
+	if word in feature_keys.keys():
+		# print(word + " >> "+word)
+		return word
 	for f, sim_words in feature_keys.items():
 		words = word.split(" ")
 		if len(sim_words) != len(words):
@@ -51,18 +54,58 @@ def get_feature_keys(word, feature_keys):
 			if words[i] in sim_words[i]:
 				check += 1
 			if check == len(words):
+				print(word + " >> " +f)
 				return f
+
 	return None
+
+def extract_features_2(data, ngram = 1, no_of_features = 5):
+	count = 0
+	extracted_features = []
+
+	count_vect = CountVectorizer(ngram_range=(1, ngram))
+	X = count_vect.fit_transform(data)
+
+	transformer = TfidfTransformer()
+	transformer.fit_transform(X)
+	indices = sorted(range(len(transformer.idf_)), key=lambda i:transformer.idf_[i])
+
+	for i in indices:
+		extracted_features.append(count_vect.get_feature_names()[i])
+
+		count += 1
+		if count == no_of_features:
+			break;
+
+	return extracted_features
 
 def get_similar_words2(word):
 	word_list = []
-	for s in wordnet.synsets(word):
-		similar_word = s.name().split('.')[0]
-		if similar_word in word:
-			for hypo in s.hyponyms():
-				tempWord = hypo.name().split(".")[0]
-				if "_" not in tempWord:
-					word_list.append(tempWord)
+
+	wordSynsets = wordnet.synsets(word)
+	if wordSynsets:
+		wordHyponyms = wordSynsets[0].hyponyms()
+		wordHypernyms = wordSynsets[0].hypernyms()
+	else:
+		wordHyponyms = None
+		wordHypernyms = None
+	for s in wordSynsets:
+		tempWord = s.name().split(".")[0]
+		if "_" not in tempWord:
+			word_list.append(tempWord)
+
+	if wordHyponyms:
+		for s in wordHyponyms:
+			tempWord = s.name().split(".")[0]
+			if "_" not in tempWord:
+				word_list.append(tempWord)
+	if wordHypernyms:
+		for s in wordHypernyms:
+			tempWord = s.name().split(".")[0]
+			if "_" not in tempWord:
+				word_list.append(tempWord)
+	word_list = set(word_list)
+	# print(word_list)
 	return word_list
 
 ###########################################
@@ -72,7 +115,12 @@ def get_similar_words2(word):
 def filter_to_feature(data, feature_key_net, ngram = 1):
 	tokens = word_tokenize(data)
 	resultString  = ""
+	skip = 0
+
 	for i in range(len(tokens) - ngram + 1):
+		if skip > 0 :
+			skip -=1
+			continue
 		feature_key = ""
 		tempString = tokens[i]
 		for j in range(1, ngram):
@@ -85,10 +133,10 @@ def filter_to_feature(data, feature_key_net, ngram = 1):
 			else:
 				resultString = resultString + " " +  tokens[i]
 			if i == len(tokens) - ngram:
-				resultString = resultString + " " + tempString
+				resultString = resultString + " " + tempString[1:]
 		else:
 			resultString = resultString + " " + feature_key
-			i += ngram - 1
+			skip = ngram - 1
 	return resultString
 
 def filter_to_feature_unigram(review, feature_key_net):
@@ -99,33 +147,36 @@ def filter_to_feature_unigram(review, feature_key_net):
 		resultString = resultString + " " + token
 	return resultString
 
-def filter_feature_sets(X, y, ngram = 2, iteration = 1):
+def filter_feature_sets(X,ngram = 2, iteration = 10, no_of_features=50):
 	for i in range(0,iteration):
 		print("start of feature_key_net generation .......")
-		vectorizer = CountVectorizer(ngram_range=(1, ngram))
-		vectorizer.fit(X, y)
-		feature_sets = vectorizer.get_feature_names()  
+		
+		feature_sets = extract_features_2(X,ngram, no_of_features) 
 		feature_key_net = generate_feature_key_capture(feature_sets)
 		newX = []
+		#please remove [0:5]
+		count = 0
 		for review in X:
-				print("before")
-				print(str(review))
 				review = filter_to_feature(str(review), feature_key_net, ngram)
-				print("After")
-				print(review)
+				if count%10==0:
+					print(count)
+				count +=1
+
 				newX.append(review)
 		return newX
 
 if __name__ == '__main__':
   # Preprocess train data
   X, y = load_preprocessed_data()
+  # X = X[0:250]
+  # y = y[0:250]
 
-  preprocess_records = filter_feature_sets(X,y,2,1)
+  preprocess_records = filter_feature_sets(X)
 
   with open('data/preprocessed_2_reviews.tsv', 'w') as preprocess_file:
     header = 'review\tsentiment\n'
     preprocess_file.write(header)
 
-    for i in range(X):
+    for i in range(len(preprocess_records)):
       preprocess_file.write('\t%s\t%i\n' %
-                            (X[i], y[i]))
+                            (preprocess_records[i], y[i]))
